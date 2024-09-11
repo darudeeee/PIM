52367a9f55b860c3298f865627a9683aefa3d5cf
jinchaeyeon <74554898+jinchaeyeon@users.noreply.github.com>
2024-09-11 15:25:55 +0900

    note 수정 중

--- src/routers/Note.js
+++ src/routers/Note.js
@@ -9,10 +9,13 @@ import "swiper/css/scrollbar";
 import { Scrollbar } from "swiper/modules";
 import SwiperCore from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import ReactMarkdown from "react-markdown";
 
 const mkdStr = `노트 초기 내용`;
 
 const Note = () => {
+  const [title, setTitle] = useState("");
+
   SwiperCore.use([Scrollbar]);
   let deviceWidth = window.innerWidth;
   const [isMobile, setIsMobile] = useState(deviceWidth <= 1200);
@@ -30,38 +33,89 @@ const Note = () => {
   return (
     <>
       {/* https://uiwjs.github.io/react-md-editor/ */}
-      {/* 모바일일땐 미리보기 버튼이나 슬라이드(에디터/프리뷰) */}
       {/* 웹 버전 디자인 + 저장버튼, 취소버튼 등등 */}
       {!isMobile ? (
         <div
           id="note"
           style={{
             display: "flex",
+            flexDirection: "column",
             height: `calc(100% - ${heights}px)`,
             width: "100%",
           }}
         >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              width: "90%",
+              height: "70px",
+            }}
+          >
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="제목을 입력하세요."
+              style={{
+                padding: "8px",
+                width: "60%",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+              }}
+            />
+          </div>
           <div class="live" data-color-mode="light">
-            {/* ul 안 화면 분할 부분들도 크기 조정 css에 class name 추가해서 */}
             <MDEditor class="markdown" value={value} onChange={setValue} />
           </div>
         </div>
       ) : (
-        // 슬라이드로 editor/preview 할건지, 미리보기 버튼을 만들건지 고민 해야됨
-        <Swiper
-          scrollbar={{
-            hide: false,
+        // 모바일은 그냥 메뉴를 없애고 search에서만 가능하게?
+        <div
+          style={{
+            display: "flex",
+            height: `calc(100% - ${heights}px)`,
+            width: "100%",
+            flexDirection: "column",
           }}
-          modules={[Scrollbar]}
-          className="mySwiper"
         >
-          <SwiperSlide>
-            <div>slide1</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>slide2</div>
-          </SwiperSlide>
-        </Swiper>
+          <div
+            style={{
+              display: "flex",
+              height: "100%",
+              width: "100%",
+              flexDirection: "column",
+            }}
+          >
+            <Swiper
+              scrollbar={{
+                hide: false,
+              }}
+              modules={[Scrollbar]}
+              className="mySwiper"
+            >
+              {/* <SwiperSlide>
+		  	<MDEditor value={value} onChange={setValue} />
+		  </SwiperSlide> */}
+              <SwiperSlide>
+                <h4 style={{ margin: "20px 5%" }}>
+                  여기에 나중에 모바일 조회 했을때 노트 제목
+                </h4>
+                <div
+                  className="markdown-preview"
+                  style={{
+                    width: "90%",
+                    height: "80%",
+                    margin: "0 5%",
+                    border: "1px solid #000",
+                  }}
+                >
+                  <ReactMarkdown>{value}</ReactMarkdown>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </div>
       )}
     </>
   );
