5a668b4544e955d9d4ceab5df37c93d5fc4c0280
darudeeee <haode0916@naver.com>
2024-10-14 11:01:49 +0900

    exercise 디자인 중

--- src/routers/Exercise.js
+++ src/routers/Exercise.js
@@ -1,7 +1,63 @@
+import { useEffect, useState } from "react";
+import { Grid } from "@mui/material";
+
 const Exercise = () => {
+  const [heights, setHeight] = useState(0);
+
+  useEffect(() => {
+    setHeight(document.getElementById("header").offsetHeight);
+  }, []);
+
   return (
     <>
-     G
+      <div
+        style={{
+          display: "flex",
+          height: `calc(100% - ${heights}px)`,
+          width: "100%",
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+            <div
+              style={{ width: "100%", height: "100%", border: "1px solid #000" }}
+            >
+              입력창
+            </div>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+          <div
+              style={{ width: "100%", height: "100%" }}
+            >
+              표시창
+            </div>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+            <div>식단</div>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+            <div>운동</div>
+          </Grid>
+        </Grid>
+
+        {/* <Grid container spacing={2}>
+  <Grid item xs={6} md={8}>
+    <Item>xs=6 md=8</Item>
+  </Grid>
+  <Grid item xs={6} md={4}>
+    <Item>xs=6 md=4</Item>
+  </Grid>
+  <Grid item xs={6} md={4}>
+    <Item>xs=6 md=4</Item>
+  </Grid>
+  <Grid item xs={6} md={8}>
+    <Item>xs=6 md=8</Item>
+  </Grid>
+</Grid> */}
+      </div>
     </>
   );
 };
