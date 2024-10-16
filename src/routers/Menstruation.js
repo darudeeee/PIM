0292ff942b551bbed6f706db8392075da2e13df0
jinchaeyeon <74554898+jinchaeyeon@users.noreply.github.com>
2024-10-16 21:21:57 +0900

    menstuation 수정 중

--- src/routers/Menstruation.js
+++ src/routers/Menstruation.js
@@ -9,9 +9,12 @@ import "swiper/css";
 import "swiper/css/scrollbar";
 import { Scrollbar } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { convertDateToStr, getDatesStartToLast, getDateDiff } from "../component/CommonFuntion";
+import {
+  convertDateToStr,
+  getDateDiff,
+  getDatesStartToLast,
+} from "../component/CommonFuntion";
 import MenstruationData from "../data/MenstruationData";
-import { avatarGroupClasses } from "@mui/material";
 
 const Menstruation = () => {
   SwiperCore.use([Scrollbar]);
@@ -31,8 +34,6 @@ const Menstruation = () => {
 
   const [start, setStart] = useState(null);
   const [end, setEnd] = useState(null);
-
-
 
   const getContent = ({ date }) => {
     // 모든? 각 타일의 날짜
@@ -68,62 +69,101 @@ const Menstruation = () => {
   };
 
   const onChange = (e) => {
-    if (start == null) {
-      setStart(e);
-      var date = new Date(e.getFullYear(), e.getMonth(), e.getDate() + 4);
-
-      let arrData = {
-        id: data.length, // 기존 0부터 시작하는 배열 뒤에 붙일 거라서
-        start: e,
-        end: date,
-        period: 5,
-        avgCycle: 0,
-        expectedDate: null,
-        fertileStart: null,
-        fertileEnd: null,
-      };
-
-      setData((prev) => [
-        // setData
-        ...prev, // 기존에 있던 내용
-        arrData, // 뒤에 새로운 내용
-      ]);
+    if (
+      data
+        .map((item) => convertDateToStr(item.start))
+        .includes(convertDateToStr(e))
+    ) {
+      //삭제
+      alert("dd");
     } else {
-      // start가 event(end)보다 과거가 아닐 시
-      if (start <= e == false) {
-        // useState로 셋팅한 것은 함수가 끝나야 사용 가능해서 e로 해야됨
-        alert("The end date of your period is earlier than the start date. Please reset it.");
+      if (start == null) {
+        setStart(e);
+        var date = new Date(e.getFullYear(), e.getMonth(), e.getDate() + 4);
+
+        let arrData = {
+          id: data.length, // 기존 0부터 시작하는 배열 뒤에 붙일 거라서
+          start: e,
+          end: date,
+          period: 5,
+          avgCycle: 0,
+          expectedDate: null,
+          fertileStart: null,
+          fertileEnd: null,
+        };
+
+        setData((prev) => [
+          // setData
+          ...prev, // 기존에 있던 내용
+          arrData, // 뒤에 새로운 내용
+        ]);
       } else {
-        setEnd(e);
-        const newData = data.map((item, index) => {
-          if (index == data.length - 1) {
-            // 이미 데이터 1개 null값과 같이 추가됨 -1(기존 하던 데이터)
-            let arrData = {
-              id: item.id, // 기존 0부터 시작하는 배열 뒤에 붙일 거라서
-              start: item.start,
-              //데이터 변환
-              end: e,
-              period: getDateDiff(item.start, e),
-              avgCycle: data
-                .filter((item) => item.avgCycle !== 0) // 평균 주기가 0이 아닐 때
-                .map((item, index) => {
-                  avgCycle += item / item.length; 
-                }),
-              expectedDate: e + avgCycle,
-              fertileStart: expectedDate - 17,
-              fertileEnd: expectedDate - 11,
-            };
-            return arrData;
-          } else {
-            return item;
-          }
-        });
-        setData(newData);
+        // start가 event(end)보다 과거가 아닐 시
+        if (start <= e == false) {
+          // useState로 셋팅한 것은 함수가 끝나야 사용 가능해서 e로 해야됨
+          alert(
+            "The end date of your period is earlier than the start date. Please reset it."
+          );
+        } else {
+          setEnd(e);
+          const newData = data.map((item, index) => {
+            if (index == data.length - 1) {
+              let avgCycle =
+                // 예외처리(data.length - 2 때문에)
+
+                // 초기값
+                data.length > 2
+                  ? data[data.length - 2].avgCycle * (data.length - 2)
+                  : 0;
+
+              // 초기 평균 주기
+              let diff =
+                data.length > 2
+                  ? getDateDiff(data[data.length - 2].end, item.start)
+                  : 28;
+
+              // 평균 주기 계산
+              let cycle = (Math.round(avgCycle) + diff) / data.length;
+              /* let cycle = (Math.round(avgCycle) + diff) / data.length;
+					  1. avgCycle = 0; 예상일 = 28;
+					  2. avgCycle = 0 + 현재start - 이전 end/ 1 = 22; 예상일 = 22일더했어
+					  3. avgCycle = 22 * 1 + 24 / 2 = 23;
+					  4. avgCycle = 23 * 2 + 현재 주기 / 3
+				  */
+              let arrData = {
+                id: item.id, // 기존 0부터 시작하는 배열 뒤에 붙일 거라서
+                start: item.start,
+                //데이터 변환
+                end: e,
+                period: getDateDiff(item.start, e),
+                avgCycle: cycle,
+                expectedDate: new Date(
+                  e.getFullYear(),
+                  e.getMonth(),
+                  e.getDate() + cycle
+                ),
+                fertileStart: new Date(
+                  e.getFullYear(),
+                  e.getMonth(),
+                  e.getDate() + cycle - 17
+                ),
+                fertileEnd: new Date(
+                  e.getFullYear(),
+                  e.getMonth(),
+                  e.getDate() + cycle - 11
+                ),
+              };
+              return arrData;
+            } else {
+              return item;
+            }
+          });
+          setData(newData);
+        }
       }
     }
   };
 
-  console.log(data);
   return (
     <>
       {!isMobile ? (
@@ -154,6 +194,12 @@ const Menstruation = () => {
               width: "30%",
             }}
           >
+            {/* 
+				카드 1 : 평균주기(data의 맨마지막 데이터의 avgCycle)
+				카드 2 : 다음 생리 예정일(data의 맨마지막 데이터의 expectedDate)
+				카드 3 : 다음 가임기 시작일(data의 맨마지막 데이터의 fertileStart)
+				카드 4 : 다음 가임기 종료일(data의 맨마지막 데이터의 fertileEnd)
+				*/}
             <Card
               sx={{
                 display: "flex",
