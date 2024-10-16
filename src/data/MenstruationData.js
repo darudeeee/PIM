b9d3074305d9eca45a53e692cf11d3b23a4fb958
jinchaeyeon <74554898+jinchaeyeon@users.noreply.github.com>
2024-10-16 14:41:29 +0900

    menstruationData 수정 중

--- src/data/MenstruationData.js
+++ src/data/MenstruationData.js
@@ -1,271 +1,45 @@
 const now = new Date();
 
 export default [
-  /*
+
   {
-    start: null, // 생리 시작일
-    averageCycle: 28, // 평균 생리 주기
-    expected: null, // 예정일
-    ovulation: null, // 배란
-    fertileStart: null, // 가임기 시작
-    fertileEnd: null, // 가임기 끝
-  },
-  */
-  {
-    id: 0,
-    title: "All Day Event very long title", 
-    allDay: true,
-    start: new Date(2024, 8, 10), //20240314 16:30:00
-    end: new Date(2024, 8, 11)
-  },
-  {
-    id: 1,
-    title: "Long Event",
-    start: new Date(2023, 3, 7),
-    end: new Date(2023, 3, 10)
-  },
-  {
-    id: 4,
-    title: "Some Event",
-    start: new Date(2023, 3, 9, 0, 0, 0),
-    end: new Date(2023, 3, 10, 0, 0, 0)
-  },
-  {
-    id: 5,
-    title: "Conference",
-    start: new Date(2023, 3, 11),
-    end: new Date(2023, 3, 13),
-    desc: "Big conference for important people"
-  },
-  {
-    id: 6,
-    title: "Meeting",
-    start: new Date(2023, 3, 12, 10, 30, 0, 0),
-    end: new Date(2023, 3, 12, 12, 30, 0, 0),
-    desc: "Pre-meeting meeting, to prepare for the meeting"
-  },
-  {
-    id: 7,
-    title: "Lunch",
-    start: new Date(2023, 3, 12, 12, 0, 0, 0),
-    end: new Date(2023, 3, 12, 13, 0, 0, 0),
-    desc: "Power lunch"
-  },
-  {
-    id: 8,
-    title: "Meeting",
-    start: new Date(2023, 3, 12, 14, 0, 0, 0),
-    end: new Date(2023, 3, 12, 15, 0, 0, 0)
-  },
-  {
-    id: 9,
-    title: "Happy Hour",
-    start: new Date(2023, 3, 12, 17, 0, 0, 0),
-    end: new Date(2023, 3, 12, 17, 30, 0, 0),
-    desc: "Most important meal of the day"
-  },
-  {
-    id: 10,
-    title: "Dinner",
-    start: new Date(2023, 3, 12, 20, 0, 0, 0),
-    end: new Date(2023, 3, 12, 21, 0, 0, 0)
-  },
-  {
-    id: 11,
-    title: "Planning Meeting with Paige",
-    start: new Date(2023, 3, 13, 8, 0, 0),
-    end: new Date(2023, 3, 13, 10, 30, 0)
-  },
-  {
-    id: 11.1,
-    title: "Inconvenient Conference Call",
-    start: new Date(2023, 3, 13, 9, 30, 0),
-    end: new Date(2023, 3, 13, 12, 0, 0)
-  },
-  {
-    id: 11.2,
-    title: "Project Kickoff - Lou's Shoes",
-    start: new Date(2023, 3, 13, 11, 30, 0),
-    end: new Date(2023, 3, 13, 14, 0, 0)
-  },
-  {
-    id: 11.3,
-    title: "Quote Follow-up - Tea by Tina",
-    start: new Date(2023, 3, 13, 15, 30, 0),
-    end: new Date(2023, 3, 13, 16, 0, 0)
-  },
-  {
-    id: 12,
-    title: "Late Night Event",
-    start: new Date(2023, 3, 17, 19, 30, 0),
-    end: new Date(2023, 3, 18, 2, 0, 0)
-  },
-  {
-    id: 12.5,
-    title: "Late Same Night Event",
-    start: new Date(2023, 3, 17, 19, 30, 0),
-    end: new Date(2023, 3, 17, 23, 30, 0)
-  },
-  {
-    id: 13,
-    title: "Multi-day Event",
-    start: new Date(2023, 3, 20, 19, 30, 0),
-    end: new Date(2023, 3, 22, 2, 0, 0)
-  },
-  {
-    id: 14,
-    title: "Today",
-    start: new Date(new Date().setHours(new Date().getHours() - 3)),
-    end: new Date(new Date().setHours(new Date().getHours() + 3))
-  },
-  {
-    id: 15,
-    title: "Point in Time Event",
-    start: now,
-    end: now
-  },
-  {
-    id: 16,
-    title: "Video Record",
-    start: new Date(2024, 3, 14, 15, 30, 0),
-    end: new Date(2024, 3, 14, 19, 0, 0)
-  },
-  {
-    id: 17,
-    title: "Dutch Song Producing",
-    start: new Date(2024, 3, 14, 16, 30, 0),
-    end: new Date(2024, 3, 14, 20, 0, 0)
-  },
-  {
-    id: 18,
-    title: "Itaewon Halloween Meeting",
-    start: new Date(2024, 3, 14, 16, 30, 0), 
-    end: new Date(2024, 3, 14, 17, 30, 0)
-  },
-  {
-    id: 19,
-    title: "Online Coding Test",
-    start: new Date(2024, 3, 14, 17, 30, 0),
-    end: new Date(2024, 3, 14, 20, 30, 0)
-  },
-  {
-    id: 20,
-    title: "An overlapped Event",
-    start: new Date(2024, 3, 14, 17, 0, 0),
-    end: new Date(2024, 3, 14, 18, 30, 0)
-  },
-  {
-    id: 21,
-    title: "Phone Interview",
-    start: new Date(2024, 3, 14, 17, 0, 0),
-    end: new Date(2024, 3, 14, 18, 30, 0)
-  },
-  {
-    id: 22,
-    title: "Cooking Class",
-    start: new Date(2024, 3, 14, 17, 30, 0),
-    end: new Date(2024, 3, 14, 19, 0, 0)
-  },
-  {
-    id: 23,
-    title: "Go to the gym",
-    start: new Date(2024, 3, 14, 18, 30, 0),
-    end: new Date(2024, 3, 14, 20, 0, 0)
-  },
-  {
-    id: 24,
-    title: "Video Record",
-    start: new Date(2025, 3, 14, 15, 30, 0),
-    end: new Date(2025, 3, 14, 19, 0, 0)
-  },
-  {
-    id: 25,
-    title: "Dutch Song Producing",
-    start: new Date(2025, 3, 14, 16, 30, 0),
-    end: new Date(2025, 3, 14, 20, 0, 0)
-  },
-  {
-    id: 26,
-    title: "Itaewon Halloween Meeting",
-    start: new Date(2025, 3, 14, 16, 30, 0),
-    end: new Date(2025, 3, 14, 17, 30, 0)
-  },
-  {
-    id: 27,
-    title: "Online Coding Test",
-    start: new Date(2025, 3, 14, 17, 30, 0),
-    end: new Date(2025, 3, 14, 20, 30, 0)
-  },
-  {
-    id: 28,
-    title: "An overlapped Event",
-    start: new Date(2025, 3, 14, 17, 0, 0),
-    end: new Date(2025, 3, 14, 18, 30, 0)
-  },
-  {
-    id: 29,
-    title: "Phone Interview",
-    start: new Date(2025, 3, 14, 17, 0, 0),
-    end: new Date(2025, 3, 14, 18, 30, 0)
-  },
-  {
-    id: 30,
-    title: "Cooking Class",
-    start: new Date(2025, 3, 14, 17, 30, 0),
-    end: new Date(2025, 3, 14, 19, 0, 0)
-  },
-  {
-    id: 31,
-    title: "Go to the gym",
-    start: new Date(2025, 3, 14, 18, 30, 0),
-    end: new Date(2025, 3, 14, 20, 0, 0)
-  },
-  {
-    id: 32,
-    title: "Video Record",
-    start: new Date(2026, 3, 14, 15, 30, 0),
-    end: new Date(2026, 3, 14, 19, 0, 0)
-  },
-  {
-    id: 33,
-    title: "Dutch Song Producing",
-    start: new Date(2026, 3, 14, 16, 30, 0),
-    end: new Date(2026, 3, 14, 20, 0, 0)
-  },
-  {
-    id: 34,
-    title: "Itaewon Halloween Meeting",
-    start: new Date(2026, 3, 14, 16, 30, 0),
-    end: new Date(2026, 3, 14, 17, 30, 0)
-  },
-  {
-    id: 35,
-    title: "Online Coding Test",
-    start: new Date(2026, 3, 14, 17, 30, 0),
-    end: new Date(2026, 3, 14, 20, 30, 0)
-  },
-  {
-    id: 36,
-    title: "An overlapped Event",
-    start: new Date(2026, 3, 14, 17, 0, 0),
-    end: new Date(2026, 3, 14, 18, 30, 0)
-  },
-  {
-    id: 37,
-    title: "Phone Interview",
-    start: new Date(2026, 3, 14, 17, 0, 0),
-    end: new Date(2026, 3, 14, 18, 30, 0)
-  },
-  {
-    id: 38,
-    title: "Cooking Class",
-    start: new Date(2026, 3, 14, 17, 30, 0),
-    end: new Date(2026, 3, 14, 19, 0, 0)
-  },
-  {
-    id: 39,
-    title: "Go to the gym",
-    start: new Date(2026, 3, 14, 18, 30, 0),
-    end: new Date(2026, 3, 14, 20, 0, 0)
-  }
+	id: 0,
+    start: new Date(2024, 8, 23), // 생리 시작
+	end: new Date(2024, 8, 28), // 생리 끝
+	period: 5,
+	avgCycle: 0, // 평균 주기(데이터가 1개면 평균주기 index 0 빼야함)
+	expectedDate: new Date(2024, 9, 25), // end + avgCycle, 날짜로 들어가야 됨
+	fertileStart: new Date(2024, 9, 8), // end + avgCycle - 17, 날짜로 들어가야 됨
+	fertileEnd: new Date(2024, 9, 14), // end + avgCycle - 11, 날짜로 들어가야 됨
+	},
+    {
+		id: 1,
+		start: new Date(2024, 9, 19), 
+		end: new Date(2024, 9, 23), 
+		period: 4, 
+		avgCycle: 22, // 현재 start - 이전 end / 갯수
+		expectedDate: new Date(2024, 10, 15),
+		fertileStart: new Date(2024, 9, 28),
+		fertileEnd: new Date(2024, 10, 4),
+	},
+	{
+		id: 2,
+		start: new Date(2024, 10, 17), 
+		end: new Date(2024, 10, 21), 
+		period: 5, 
+		avgCycle: 23, // 이번달 주기 24일 + 저번달 22 / 2개
+		expectedDate: new Date(2024, 11, 14), 
+		fertileStart: new Date(2024, 10, 28), 
+		fertileEnd: new Date(2024, 11, 3), 
+		},
+		{
+			id: 3,
+			start: new Date(2024, 11, 20), 
+			end: new Date(2024, 11, 26), 
+			period: 7, 
+			avgCycle: 25.33, // 나중에 반올림 해야함 22 + 24 + 30 / 3
+			expectedDate: new Date(2024, 12, 21), 
+			fertileStart: new Date(2024, 12, 4), 
+			fertileEnd: new Date(2024, 12, 10), 
+			},
 ];
